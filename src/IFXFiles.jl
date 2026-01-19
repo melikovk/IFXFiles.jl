f23e67324583d82ffa3d39643ac5658f5306c5f1
Kamran Melikov <melikovk@gmail.com>
2026-01-19 11:19:27 -0500

    Add read function
    On branch main
    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    modified:   src/IFXFiles.jl

--- src/IFXFiles.jl
+++ src/IFXFiles.jl
@@ -1,5 +1,45 @@
 module IFXFiles
 
-# Write your package code here.
+function read(stream::IO)::DataFrame
+    metadata = Dict{String,String}()
+    # read header into metadata dictionary
+    for line in eachline(stream)
+        stripped = strip(line)
+        if isempty(stripped)
+            continue
+        elseif startswith(stripped, "[Data]")
+            break # [Data] markers end the header section
+        else
+            push!(metadata, Pair(split(stripped, "=", limit=2, keepempty=true)...))
+        end
+    end
+
+    if haskey(metadata, "Columns")
+        column_names = String.(strip.(split(metadata["Columns"], ',')))
+    else
+        error("Could not find Columns= line in header")
+    end
+
+    # Read the CSV starting from the data section
+    df = CSV.read(
+        stream,
+        DataFrame;
+        delim=' ',
+        ignorerepeated=true,  # Handle multiple spaces
+        header=column_names,
+        comment="#"
+    )
+    # Add metadata to the Dataframe
+    for (key, value) in metadata
+        metadata!(df, key, value, style=:note)
+    end
+    return df
+end
+
+function read(filepath::AbstractString)
+    open(filepath, "r") do file
+        read(file)
+    end
+end
 
 end
